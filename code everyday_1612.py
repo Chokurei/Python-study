0f2db112bd7e3dd74424bb56f3dd67c5bd68368c
Guo Zhiling <guozhilingty@gmail.com>
2016-12-17 01:12:58 +0900

    Update code everyday_1612.py

--- code everyday_1612.py
+++ code everyday_1612.py
@@ -1,3 +1,6 @@
 #Make a script both importable and executable, name can be replaced
 __name__ =='__main__'
-
+#list
+#https://docs.python.org/2/tutorial/datastructures.html
+del popleft
+stack and queue in list
