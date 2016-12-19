6dc4a82ae9c95a1414ccbbaf745f262ef8cd2153
Guo Zhiling <guozhilingty@gmail.com>
2016-12-20 01:09:54 +0900

    Update code everyday_1612.py

--- code everyday_1612.py
+++ code everyday_1612.py
@@ -2,5 +2,19 @@
 __name__ =='__main__'
 #list
 ##https://docs.python.org/2/tutorial/datastructures.html
+from collections import deque
 del popleft
 stack and queue in list
+
+#numpy
+##https://docs.scipy.org/doc/numpy-dev/user/quickstart.html
+b.cumsum()
+a[:6:2] = a[0:6:2] = -1000 
+np.vstack((a,b)) np.hstack((a,b))
+np.column_stack((a,b)) 
+##The function column_stack stacks 1D arrays as columns into a 2D array. It is equivalent to vstack only for 1D arrays:
+np.hsplit(a,3) np. hsplit((3,4)) np.vsplit(a,3)
+np.random.random((3,3))
+a = np.arange(12).reshape(3,4) b = a > 4   a[b]
+## The dots (...) represent as many colons as needed to produce a complete indexing tuple. 
+x[1,2,...] = x[1,2,:,:,:],
