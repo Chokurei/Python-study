704a0a386a5014622715164f49de86be581bb919
Guo Zhiling <guozhilingty@gmail.com>
2016-12-25 00:39:10 +0900

    Update code everyday_1612.py

--- code everyday_1612.py
+++ code everyday_1612.py
@@ -1,11 +1,43 @@
+1. System:
+# bash
+export PATH="/home/kaku/anaconda3/bin:$PATH"   
+$PATH
+# 
+which python
+ls -l /usr/bin/python
+ln -ls /usr/bin/python2.6       /usr/bin/python
+
+
+
 #Make a script both importable and executable, name can be replaced
 __name__ =='__main__'
+
+
+
+2. python basic
 #list
 ##https://docs.python.org/2/tutorial/datastructures.html
 from collections import deque
 del popleft
 stack and queue in list
 
+#dictionary
+##https://docs.python.org/2/tutorial/datastructures.html
+basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+fruit = set(basket)               # create a set without duplicates
+a = set('abracadabra')
+b = set('alacazam')
+a - b      a | b        a & b    a ^ b   
+tel = {'jack': 4098, 'sape': 4139}
+dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+dict(sape=4139, guido=4127, jack=4098)
+tel['guido'] = 4127
+tel['jack']
+del tel['sape']
+tel.keys()
+'guido' in tel
+
+3. modules
 #numpy
 ##https://docs.scipy.org/doc/numpy-dev/user/quickstart.html
 b.cumsum()
@@ -18,3 +50,4 @@ np.random.random((3,3))
 a = np.arange(12).reshape(3,4) b = a > 4   a[b]
 ## The dots (...) represent as many colons as needed to produce a complete indexing tuple. 
 x[1,2,...] = x[1,2,:,:,:],
+
